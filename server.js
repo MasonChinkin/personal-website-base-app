5be595387f6b5e43d5d3352a784d59312f792c39
Mason Chinkin <mchinkin@gopro.com>
2019-11-27 17:21:55 -0800

    Base

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ const path = require('path');
 
 // App
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/reactapp', express.static('build'));
 
-app.set('trust proxy', function (ip) {
-  console.log('ip: ', ip)
-  return true;
-})
-
-app.get('/ping', (req, res) => {
-  console.log('ROUTE: /ping');
-  return res.send('pong');
-});
-
-app.get('/', (req, res) => {
+app.get('/reactapp', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = 80;
+app.get('/reactapp/ping', (req, res) => {
+  return res.send('pong');
+});
+
+const PORT = 9999;
 app.listen(PORT);
 console.log(`Listening on port ${PORT}`);
