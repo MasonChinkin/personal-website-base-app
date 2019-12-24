ce19088746c60d8124cbccf95432f973df531195
Mason Chinkin <mchinkin@gopro.com>
2019-12-24 14:10:44 -0800

    Add deep linking

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 app.use('/baseapp', express.static('build'));
 
-app.get('/baseapp', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.get('/baseapp/ping', (req, res) => {
   return res.send('pong');
+});
+
+app.get('/baseapp/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = 9999;
--- src/components/CreateReactApp.jsx
+++ src/components/CreateReactApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 
-function App() {
+function CreateReactApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default CreateReactApp;
